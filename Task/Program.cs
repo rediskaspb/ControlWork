24591352b74c57eee5302357f8ef76c4582150b7
Anastasiya <9399023@mail.ru>
2023-01-25 21:45:21 +0300

    add task solution

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,5 +3,27 @@
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-
-Console.WriteLine("Hello, World!");
+string[] array = {"12hj", "123456", "12v", "2U", "Frd458j", "8="};
+var result = new string[array.Length];
+void Array3symb(string[] array, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        result[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Array3symb(array, result);
+PrintArray(result);
